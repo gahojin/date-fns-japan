88bb5d7ac77a7ef53861b186e56c18743f13175b
Zenichi Amano <zenichi.amano@gahojin.co.jp>
2024-11-22 18:22:41 +0900

    chore: エントリにワイルドカードを使用し、個別パッケージを列挙しなくて良いよう修正
    Signed-off-by: Zenichi Amano <zenichi.amano@gahojin.co.jp>

--- tsup.config.ts
+++ tsup.config.ts
@@ -12,14 +12,7 @@ const banner = `/**
 
 export default defineConfig((options) => {
   const commonOptions: Partial<Options> = {
-    entry: [
-      'src/index.ts',
-      'src/addJapan/index.ts',
-      'src/isAfterDay/index.ts',
-      'src/isBeforeDay/index.ts',
-      'src/isWithinGtfsCalendar/index.ts',
-      'src/normalizeDuration/index.ts',
-    ],
+    entry: ['src/**/index.ts'],
     sourcemap: true,
     treeshake: 'recommended',
     banner: {
