662fd34d86f249039b579de835df7abb2cde1b06
Zenichi Amano <zenichi.amano@gahojin.co.jp>
2026-07-15 11:17:17 +0900

    build: rolldown設定修正
    コメント出力(アノテーション、leagal)を有効にし、package.jsonからのentryのネスト対応
    
    Signed-off-by: Zenichi Amano <zenichi.amano@gahojin.co.jp>

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -3,32 +3,33 @@ import { defineConfig } from 'rolldown'
 import { dts } from 'rolldown-plugin-dts'
 import pkg from './package.json'
 
-const inputIndexFiles = Object.keys(pkg.exports)
-  .filter((entry) => !entry.endsWith('.json'))
-  .map((entry) => {
-    const name = path.basename(entry)
-    const chunkName = name === '.' ? 'index' : `${name}/index`
-    return [chunkName, path.resolve('src', name, 'index.ts')]
-  })
+const getInputIndexFiles = (exports: Record<string, unknown>) => {
+  const entries = Object.keys(exports)
+    .filter((entry) => !entry.endsWith('.json'))
+    .map((entry) => {
+      const name = entry.replace(/^\.\/?/, '') || '.'
+      const chunkName = name === '.' ? 'index' : `${name}/index`
+      return [chunkName, path.resolve('src', name, 'index.ts')]
+    })
+  return Object.fromEntries(entries)
+}
 
-export default defineConfig([
-  {
-    external: ['date-fns', /^date-fns\//],
-    treeshake: {
-      moduleSideEffects: false,
-    },
-    optimization: {
-      inlineConst: true,
-    },
-    experimental: {
-      nativeMagicString: true,
-    },
-    input: Object.fromEntries(inputIndexFiles),
-    output: [{ dir: 'dist', format: 'es', sourcemap: true, cleanDir: true, comments: false }],
-    plugins: [
-      dts({
-        oxc: true,
-      }),
-    ],
+export default defineConfig({
+  external: ['date-fns', /^date-fns\//],
+  treeshake: {
+    moduleSideEffects: false,
   },
-])
+  optimization: {
+    inlineConst: true,
+  },
+  experimental: {
+    nativeMagicString: true,
+  },
+  input: getInputIndexFiles(pkg.exports),
+  output: [{ dir: 'dist', format: 'es', sourcemap: true, cleanDir: true, comments: { annotation: true, jsdoc: false, legal: true } }],
+  plugins: [
+    dts({
+      oxc: true,
+    }),
+  ],
+})
